3d45de7ffd5e5463b7ca9c970bec5161c18013a5
amni <amni2015@gmail.com>
2016-09-24 14:32:32 -0700

    adding splitting, localization, and animation

--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -10,58 +10,134 @@ import UIKit
 
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipSegment: UISegmentedControl!
+    
+    @IBOutlet weak var billCurrencyLabel: UILabel!
+    @IBOutlet weak var tipCurrencyLabel: UILabel!
+    @IBOutlet weak var totalCurrencyLabel: UILabel!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var plusLabel: UILabel!
+    
+    @IBOutlet weak var perPersonLabel: UILabel!
+    
+    @IBOutlet weak var minusLabel: UILabel!
+    @IBOutlet weak var numberPplLabel: UILabel!
+    var numberPpl = 1
+    var animatedLabels:[UILabel] = [UILabel]()
+    
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animatedLabels = [splitLabel, minusLabel, plusLabel, perPersonLabel, numberPplLabel]
+        
+        for label in animatedLabels{
+            label.center.x  -= view.bounds.width
+        }
+        
         let defaults = NSUserDefaults.standardUserDefaults()
         if defaults.objectForKey("billText") != nil {
             billField.text = defaults.objectForKey("billText") as! String
         }
         billField.becomeFirstResponder()
-
+        let locale = NSLocale.currentLocale()
+        let currencySymbol = locale.objectForKey(NSLocaleCurrencySymbol) as! String
+        
+        billCurrencyLabel.text = currencySymbol
+        tipCurrencyLabel.text = currencySymbol
+        totalCurrencyLabel.text = currencySymbol
+        
+        let tap = UITapGestureRecognizer(target: self, action: Selector("onTapOnTotalLabel:"))
+        totalLabel.addGestureRecognizer(tap)
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
     override func viewWillAppear(animated: Bool) {
         let defaults = NSUserDefaults.standardUserDefaults()
         let intValue = defaults.integerForKey("segmentIndex")
         tipSegment.selectedSegmentIndex = intValue
         calculateTip(self)
     }
-
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    @IBAction func animateText(sender: AnyObject) {
+    @IBAction func onTapOnTotalLabel(sender:UITapGestureRecognizer) {
+       numberPpl += 1
+       self.animatePlusSplitLabel()
+    }
+    
+    
+    @IBAction func onTapOnMinusLabel(sender: AnyObject) {
+        numberPpl-=1
+        self.animateMinusSplitLabel()
+
+    }
+    
+    func animatePlusSplitLabel(){
+        self.calculateTip(nil)
+        if (numberPpl == 2) {
+            UIView.animateWithDuration(0.5, animations: {
+                for label in self.animatedLabels{
+                    label.center.x += self.view.bounds.width
+                }
+            })
+        }
+
+    }
+    
+    func animateMinusSplitLabel(){
+        self.calculateTip(nil)
+        if (numberPpl <= 1) {
+            UIView.animateWithDuration(0.5, animations: {
+                for label in self.animatedLabels{
+                    label.center.x -= self.view.bounds.width
+                }
+            })
+        }
+    }
+    
+    
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+    
+    
+    func calculateSplit(total:Double){
+        numberPplLabel.text = String(numberPpl)
+        splitLabel.text = String(format: "%.2f", total/Double(numberPpl)    )
         
     }
     
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    }
-
-
-    @IBAction func calculateTip(sender: AnyObject) {
+    
+    @IBAction func calculateTip(sender: AnyObject?) {
         let tipPercentages = [0.15, 0.18, 0.20]
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages [tipSegment.selectedSegmentIndex]
         let total = bill + tip
         
+        calculateSplit(total)
+        
         tipLabel.text = String(format: "%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = String(format: "%.2f", total)
         
         let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setObject(billField.text, forKey: "billText")
         defaults.synchronize()
     }
-
+    
 }
 
